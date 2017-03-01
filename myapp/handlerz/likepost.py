2723c09702575f7d03d2d6aa252b002e3cb08b15
Matt <matthewrhedlund@gmail.com>
2017-03-01 12:34:06 -0500

    Update docstrings and clean up formatting

--- myapp/handlerz/likepost.py
+++ myapp/handlerz/likepost.py
@@ -7,13 +7,18 @@ from myapp.functions.decorators import user_logged_in, post_exists
 
 class LikePost(Handler):
 
-    """Do the 'Like' Functionality."""
+    """Add a 'Like' to a post on the users behalf."""
 
     @user_logged_in
-    # does not own post
-    # has not liked before (no likes in db for them currently)
     @post_exists
     def get(self, post_id):
+        """Add a like to the 'Likez' database associated with the post.
+
+        Verify that the user is logged in, that the post exists, that they
+        aren't the post creator, and that the user hasn't liked the post
+        previously before allowing the database to update to the new value.
+
+        """
         key = db.Key.from_path("Post", int(post_id))
         post = db.get(key)
         current_user = (self.request.cookies.get("user_id")).split("|")[0]
@@ -24,12 +29,9 @@ class LikePost(Handler):
                             li.does_like):
                 self.redirect("/blog/%s" % str(post_id))
         if self.read_secure_cookie("user_id") != post.creator:
-            # current_user = (self.request.cookies.get("user_id")).split("|")[0]
             current_name = (self.request.cookies.get("user")).split("|")[0]
             l = Likez(creator=current_user, name=current_name,
                       post_id=post_id, does_like=True)
             l.put()  # sends Likez object "l" to the GAE datastore
             sleep(.2)
         self.redirect("/blog/%s" % str(post_id))
-        # else:
-        #     self.redirect("/blog/login")
--- myapp/handlerz/newpost.py
+++ myapp/handlerz/newpost.py
@@ -16,19 +16,15 @@ class NewPost(Handler):
 
     @user_logged_in
     def get(self):
-        """Call method that renders the newpost page."""
-
-        # if self.read_secure_cookie("user_id"):
+        """Call method that renders the newpost page if user is logged in."""
         self.render_newpost()
-        # else:
-        #     self.redirect("/blog/login")
 
     @user_logged_in
     def post(self):
         """Coditionally create new blog posts.
 
         Accept user input for blog post subject and content, and if the user
-        is signed in (checked for be examining cookies), and they have
+        is signed in (checked for by examining cookies), and they have
         included both a subject and content, create a new post object in the
         Post entity. If the visitor is not logged in, redirect them to the
         login page to do so. If the user doesn't enter both a subject and
@@ -38,18 +34,13 @@ class NewPost(Handler):
         subject = self.request.get("subject")
         content = self.request.get("content")
         if subject and content:
-            # name1 = self.request.cookies.get("user")
             name = (self.request.cookies.get("user")).split("|")[0]
-            # creator1 = self.request.cookies.get("user_id")
             creator = (self.request.cookies.get("user_id")).split("|")[0]
             p = Post(subject=subject, content=content, creator=creator,
                      name=name)
             p.put()  # sends Post object "p" to the GAE datastore
             sleep(.2)
             self.redirect("/blog/%s" % str(p.key().id()))
-        # elif not self.read_secure_cookie("user"):
-        #     error = "You must be logged in to create a new post."
-        #     self.render_newpost(subject, content, error)
         else:
             error = ("You need to enter both a Subject and Content to create "
                      "a new post.")
